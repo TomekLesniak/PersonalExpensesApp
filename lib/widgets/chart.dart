5a084d70559a26d392bd9d52314506174ff53f23
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-19 18:05:54 +0200

    Draw expenses on chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -3,35 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import '../widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
   Chart(this.recentTransactions);
 
-
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
       var totalSum = 0.0;
 
-      for(var i = 0; i < recentTransactions.length; i++) {
-        if(_isSameDay(recentTransactions[i].date, weekDay)) {
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (_isSameDay(recentTransactions[i].date, weekDay)) {
           totalSum += recentTransactions[i].amount;
-         }
         }
+      }
 
       print(DateFormat.E().format(weekDay));
       print(totalSum);
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });
   }
 
   bool _isSameDay(DateTime dateFirst, DateTime dateSecond) {
-    return dateFirst.day == dateSecond.day
-         && dateFirst.month == dateSecond.month
-         && dateFirst.year == dateSecond.year;
+    return dateFirst.day == dateSecond.day &&
+        dateFirst.month == dateSecond.month &&
+        dateFirst.year == dateSecond.year;
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum += item['amount'];
+    });
   }
 
   @override
@@ -40,10 +49,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: [
-
-        ],
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((day) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                day['day'],
+                day['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (day['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
